999bf4ea66c6fbb55d45758c50c77e9325d27882
dominictrier <dominictrier@users.noreply.github.com>
2024-12-23 17:39:47 +0700

    add ParentNode EQ and REPR Tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -9,17 +9,17 @@ class TestHTMLNODE(unittest.TestCase): #node = HTMLNODE("a", "this is an example
 
     # Test HTMLNODE
 
-    def test_eq(self):
+    def test_HTMLNODE_eq(self):
         node = HTMLNODE("a", "this is an example text", None)
         node2 = HTMLNODE("a", "this is an example text", None)
         self.assertEqual(node, node2)
 
-    def test_noteq(self):
+    def test_HTMLNODE_noteq(self):
         node = HTMLNODE("a", "this is an example text", None)
         node2 = HTMLNODE("b", "this is an example text", None)
         self.assertNotEqual(node, node2)
 
-    def test_initialization_with_all_attributes(self):
+    def test_HTMLNODE_initialization_with_all_attributes(self):
         node = HTMLNODE("a", "link text", [HTMLNODE("span", "child")], {"href": "https://example.com"})
         assert node.tag == "a"
         assert node.value == "link text"
@@ -28,37 +28,37 @@ class TestHTMLNODE(unittest.TestCase): #node = HTMLNODE("a", "this is an example
         assert node.children[0].tag == "span"
         assert node.props["href"] == "https://example.com"
 
-    def test_initialization_with_defaults(self):
+    def test_HTMLNODE_initialization_with_defaults(self):
         node = HTMLNODE()
         assert node.tag is None
         assert node.value is None
         assert node.children is None
         assert node.props is None
 
-    def test_repr(self):
+    def test_HTMLNODE_repr(self):
         node = HTMLNODE("a", "this is an example text", None, {"href": "https://www.google.com", "target": "_blank"})
         assert repr(node) == "HTMLNODE(a, this is an example text, None, href: https://www.google.com, target: _blank)"
 
     # Test LeafNode
 
-    def test_leafnode_eq(self):
+    def test_LeafNode_eq(self):
         node = LeafNode("p", "this is an example text", None)
         node2 = LeafNode("p", "this is an example text", None)
         self.assertEqual(node, node2)
 
-    def test_leafnode_repr_prop(self):
+    def test_LeafNode_repr_prop(self):
         node = LeafNode("p", "this is an example text", {"href": "https://www.google.com", "target": "_blank"})
         assert repr(node) == "LeafNode(p, this is an example text, href: https://www.google.com, target: _blank)"
 
-    def test_leafnode_repr_noprop(self):
+    def test_LeafNode_repr_noprop(self):
         node = LeafNode("a", "example link!")
         assert repr(node) == "LeafNode(a, example link!, None)"
 
-    def test_leafnode_to_html_text(self):
+    def test_LeafNode_to_html_text(self):
         node = LeafNode("b", "example text").to_html()
         assert node == "<b>example text</b>"
         
-    def test_leafnode_to_html_a(self):
+    def test_LeafNode_to_html_a(self):
         node = LeafNode("a", "example text", {"href": "https://www.example.com"})
         result = node.to_html()
         assert result == '<a href="https://www.example.com">example text</a>'
@@ -78,6 +78,40 @@ class TestHTMLNODE(unittest.TestCase): #node = HTMLNODE("a", "this is an example
                 )
         result = node.to_html()
         assert result == '<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>'
+
+    
+    def test_ParentNode_eq(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+                ],
+                )
+        node2 = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+                ],
+                )
+        self.assertEqual(node, node2)
+
+
+    def test_ParentNode_repr(self):
+        node = ParentNode("p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+                ],
+                )
+        assert repr(node) == "ParentNode(p, [LeafNode(b, Bold text, None), LeafNode(None, Normal text, None), LeafNode(i, italic text, None), LeafNode(None, Normal text, None)], None)"
 
 
 
