e48e859351ddd2b8752e8dd7a6b72aa341fde1ee
dominictrier <dominictrier@users.noreply.github.com>
2025-01-04 12:57:05 +0700

    finished delimiter function and tests

--- src/delimiter.py
+++ src/delimiter.py
@@ -2,29 +2,26 @@ from textnode import TextNode, TextType
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    delimiter_trigger = False
 
-    delimiter_to_texttype = {
-    '`': TextType.CODE,
-    '**': TextType.BOLD,
-    '*': TextType.ITALIC
-    }
+    new_nodes = []
 
     for node in old_nodes:
-        if text_type != TextType.NORMAL:
+        if node.text_type != TextType.NORMAL:
             new_nodes.append(node)
+            continue
+            
         node_text = node.text
         split_text = node_text.split(delimiter)
-        for text in split_text:
-            if delimiter_trigger == False:
+        
+        for i, text in enumerate(split_text):
+            if i % 2 == 0:
                 new_nodes.append(TextNode(text, TextType.NORMAL))
-                delimiter_trigger = True
             else:
-                text_type = delimiter_to_texttype.get(delimiter, TextType.NORMAL)
                 new_nodes.append(TextNode(text, text_type))
-                delimiter_trigger = False
-    if delimiter_trigger == True:
-        raise Exception("incomplete delimiter sequence")
+
+    # After loops
+    if len(split_text) % 2 == 0:
+        raise Exception("Incomplete delimiter sequence")
+
 
     return new_nodes      
--- src/test_delimiter.py
+++ src/test_delimiter.py
@@ -5,22 +5,22 @@ from textnode import TextNode, TextType
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
-    # def test_multiple_split(self):
-    #     old_nodes = [
-    #         TextNode("This has a **bold** and a *italic* word", TextType.NORMAL)
-    #         ]
-    #     delimiter = '**'
-    #     text_type = TextType.BOLD
+    def test_multiple_split(self):
+        old_nodes = [
+            TextNode("This has a **bold** and a *italic* word", TextType.NORMAL)
+            ]
+        delimiter = '**'
+        text_type = TextType.BOLD
 
-    #     result = split_nodes_delimiter(old_nodes, delimiter, text_type)
+        result = split_nodes_delimiter(old_nodes, delimiter, text_type)
 
-    #     expected = [
-    #         TextNode("This has a ", TextType.NORMAL),
-    #         TextNode("bold", TextType.BOLD),
-    #         TextNode(" and a *italic* word", TextType.NORMAL)
-    #     ]
+        expected = [
+            TextNode("This has a ", TextType.NORMAL),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" and a *italic* word", TextType.NORMAL)
+        ]
 
-    #     self.assertEqual(result,expected, "Test failed: did not match expected result")
+        self.assertEqual(result,expected, "Test failed: did not match expected result")
 
 
     def test_simple_split(self):
@@ -35,7 +35,6 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             TextNode("code", TextType.CODE),
             TextNode(" segment", TextType.NORMAL)
     ]
-        print (result)
         self.assertEqual(result, expected, "Test failed: did not match expected result")
 
 
