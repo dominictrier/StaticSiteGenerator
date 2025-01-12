a98771faf7a70248a573d1b7ebe71299552ff9b9
dominictrier <dominictrier@users.noreply.github.com>
2025-01-12 20:51:19 +0700

    updated markup to hmtlnode function

--- src/markdown_to_htmlnode.py
+++ src/markdown_to_htmlnode.py
@@ -1,8 +1,13 @@
 from markdown_to_blocks import markdown_to_blocks
+from block_to_blocktype import block_to_blocktype
 
 
 def markdown_to_html_node(markdown):
     markdown_blocks = markdown_to_blocks(markdown)
+    for block in markdown_blocks:
+        block_type = block_to_blocktype(block)
+        print(block_type)
+        print(block)
 
 
 
@@ -30,11 +35,11 @@ def markdown_to_html_node(markdown):
 
 test_markdown = """# This is a heading
 
-        This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
 
-        * This is the first list item in a list block
-        * This is a list item
-        * This is another list item"""
+* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
 
 test = markdown_to_html_node(test_markdown)
 print(test)
