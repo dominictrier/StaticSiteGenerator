08e1ec96f4e57c653970b62bb6119796a9f0b027
dominictrier <dominictrier@users.noreply.github.com>
2025-01-13 19:46:00 +0700

    update markdown_to_htmlnode function

--- src/markdown_to_htmlnode.py
+++ src/markdown_to_htmlnode.py
@@ -1,20 +1,41 @@
 from markdown_to_blocks import markdown_to_blocks
 from block_to_blocktype import block_to_blocktype
+from htmlnode import HTMLNODE, LeafNode, ParentNode
 
 
 def markdown_to_html_node(markdown):
+    htmlnodes = []
     markdown_blocks = markdown_to_blocks(markdown)
     for block in markdown_blocks:
         block_type = block_to_blocktype(block)
-        print(block_type)
-        print(block)
+        if block_type == "heading":
+            result = header_to_html(block)
+            htmlnodes.append(result)
+        if block_type == "code":
+            result = 
 
 
 
 
 
 
-    return markdown_blocks
+
+
+    return "nothing"
+
+
+
+def header_to_html(markdown):
+    text = markdown.split(" ", 1)
+    amount = text[0].count("#")
+    node = LeafNode(f'h{amount}', text[1])
+    
+    return node
+
+
+def code_to_html(markdown):
+    text = 
+    
 
 
 
@@ -33,7 +54,7 @@ def markdown_to_html_node(markdown):
 
 # testing area
 
-test_markdown = """# This is a heading
+test_markdown = """### This is a heading
 
 This is a paragraph of text. It has some **bold** and *italic* words inside of it.
 
