875db34b0b1f765340208bc95c5583c2f459bc3b
dominictrier <dominictrier@users.noreply.github.com>
2025-01-24 22:59:34 +0700

    finished main function

--- src/main.py
+++ src/main.py
@@ -2,23 +2,19 @@ import os
 import shutil
 
 
-def assure_empty(dir: str):
-    if not dir:
-        raise Exception("no directory provided")
+def clean_dir(dir: str):
     if not os.path.exists(dir):
-        raise Exception("directory does not exists")
+        raise Exception("directory does not exist")
     dir_content = os.listdir(dir)
-    if dir_content == []:
-        print("directory is empty .. nothing todo")
     for item in dir_content:
         path = os.path.join(dir, item)
         if os.path.isfile(path):
             os.remove(path)
         if os.path.isdir(path):
             shutil.rmtree(path)
-    print(f"content of directory {dir} sucessfully deleted")
 
 
+# helper print function
 def print_files_in_dir(dir: str):
     dir_content = os.listdir(dir)
     for item in dir_content:
@@ -26,29 +22,47 @@ def print_files_in_dir(dir: str):
         print(full_path)
 
 
-def move_folder_cotent(source, target):
-    if target.path.exists():
-        target_dir_content = target.listdir()
-    if len(target_dir_content) > 0:
-        pass
+def recursive_copy_content(source: str, target: str):
+    dir_content = os.listdir(source)
+    for item in dir_content:
+        path_source = os.path.join(source, item)
+        path_target = os.path.join(target, item)
+        if os.path.isfile(path_source):
+            shutil.copy(path_source, path_target)
+        if os.path.isdir(path_source):
+            os.mkdir(os.path.join(target, item))
+            recursive_copy_content(os.path.join(source, item), os.path.join(target, item))
 
-def assure_target(dir: str):
+
+def assure_exists(dir: str, create: str = "no"):
     if os.path.exists(dir):
-        print("directory exists, nothing todo")
-    if not os.path.exists(dir):
-        print("directory does not exists ... creating")
-        os.mkdir(dir)
-        
+        return True
+    else:
+        if create == "yes":
+            os.mkdir(dir)
+            return True  
+    return False
+
+
+def dir_isempty(dir: str):
+    dir_content = os.listdir(dir)
+    if not dir_content:
+        return True
+    return False
+
 
 def main():
-    static = "./static/"
-    public = "./public/"
-    testing = "./testing"
-    assure_target(public)
-    assure_empty(public)
-
-
-
+    source = "./static/"
+    target = "./public/"
+    assure_exists(target, "yes")
+    if not assure_exists(source):
+        raise Exception("no source directory")
+    if dir_isempty(source):
+        raise Exception("source is empty")
+    if not dir_isempty(target):
+        clean_dir(target)
+    recursive_copy_content(source, target)
+    clean_dir(source)
 
 
 if __name__ == "__main__":
--- src/scrapboard.py
+++ src/scrapboard.py
@@ -1,17 +1,7 @@
-# text = """> this is test line 1
-# > this is test line 2
-# > this is test line 3"""
-# text_split = text.splitlines()
+list = []
 
 
-
-# for line in text_split:
-#     print(line)
-#     text_body = line.split(" ", 1)
-#     print(text_body[1])
-
-
-test = ["a","b","c"]
-
-for count, letter in enumerate(test):
-    print(count, letter)
+if not list:
+    print("empty")
+else:
+    print("not empty")
